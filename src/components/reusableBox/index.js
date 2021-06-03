458a8b622e58c2d46f898aba7e0516e87faa733c
mohitchauhan07appinventiv <mohit.chauhan@appinventiv.com>
2021-06-03 11:45:45 +0530

    Error Removal procedure

---
+++ src/components/reusableBox/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class ReusablaBox extends React.Component{
+    render(){
+        return(
+            <div className={'itemContainer'}>
+                <h3>{"First Componenet"}}</h3>
+                <button>{"click me"}</button>
+            </div>
+        );
+    }
+}
